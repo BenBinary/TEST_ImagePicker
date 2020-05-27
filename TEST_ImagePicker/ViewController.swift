d8d5c598971082a8f7f0c9625a3b76c92167113a
Bene <bk191414@gmail.com>
2020-05-27 15:40:07 +0200

    Initial Commit

---
+++ TEST_ImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TEST_ImagePicker
+//
+//  Created by Benedikt Kurz on 27.05.20.
+//  Copyright © 2020 Benedikt Kurz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
